e75d2a2c2c5cd4204c613364ecf206ebc79af7a9
Juliano Costa <88951981+JlnCosta82@users.noreply.github.com>
2021-08-19 18:07:49 -0300

    Add files via upload

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,13 +1,13 @@
 import link from 'next/link';
 
-function Home(){
+function Sobre(){
 	return (<div>
 	<h1>project-futweb v1-alfa</h1>
-	<link href="/sobre" >
-        <a>Acessar página Sobre</a>
+	<link href="/" >
+        <a>Acessar página Home</a>
     </link>
 	</div>)
 }
 
 
-export default Home
+export default Sobre
